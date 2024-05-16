b71b89911611467ee30719ce5acaf7af456c682a
Francisco Montenegro <franciscomontenegro22@gmail.com>
2024-05-16 17:51:03 -0300

    added model and migration postulantes

---
+++ database/migrations/2024_05_16_203938_create_postulantes_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('postulantes', function (Blueprint $table) {
+            Schema::create('postulantes', function (Blueprint $table) {
+                $table->id();
+                $table->string('apellido');
+                $table->string('nombre');
+                $table->string('dni');
+                $table->date('fecha_nacimiento');
+                $table->date('lugar_nacimiento');
+                $table->string('domicilio');
+                $table->string('localidad');
+                $table->string('email');
+                $table->string('telefono');
+                $table->string('foto_postulante');
+                $table->string('estado_civil');
+                $table->string('nacionalidad');
+                $table->string('cargo_ocupa');
+                $table->timestamps();
+        });
+        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('postulantes');
+    }
+};
